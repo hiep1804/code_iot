ecc632d7134c1fa7f839f3521170c6d73b5f1b78
nghiep <nghiep010804@gmail.com>
2025-10-17 16:53:44 +0700

    thêm sensor và báo thức

--- src/main.cpp
+++ src/main.cpp
@@ -6,43 +6,60 @@
 const char *ssid = "nghiep";
 const char *password = "12345678";
 
-String serverUrl = "http://192.168.137.1:8080/data"; // địa chỉ máy tính + port server
+String serverUrl = "http://192.168.137.1:8080"; // địa chỉ máy tính + port server
 
 int redPin = 14;
 int greenPin = 12;
 int bluePin = 13;
 
+int light_sensor=33;
+
+int r = 0;
+int g = 0;
+int b = 0;
+int level = 4;
+
+long long begin_time = 0;
 void setup()
 {
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  Serial.print("Đang kết nối Wi-Fi...");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("Đã kết nối Wi-Fi!");
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
+  pinMode(light_sensor,INPUT);
 }
 
 // Hàm trộn màu RGB (0–255)
 void setColor(int r, int g, int b)
 {
+  r = r * (float)0.38 * (10 + level - 4) / 10;
+  g = g * (float)0.19 * (10 + level - 4) / 10;
+  b = b * (float)1 * (10 + level - 4) / 10;
+  Serial.printf("r=%d, g=%d, b=%d, level=%d\n", r, g, b, level);
   analogWrite(redPin, r);
   analogWrite(greenPin, g);
   analogWrite(bluePin, b);
 }
-
+void statusFromSensor(){
+  int val=analogRead(light_sensor);
+  setColor(val/16,val/16,val/16);
+  delay(1000);
+}
 void loop()
 {
+  long long milis = millis();
+  //getColorFromServer();
+  statusFromSensor();
+  long long period = millis() - milis;
+  long long t = period / 1000;
+  delay((t + 1) * 1000 - period); // gửi request mỗi 1 giây
+}
+void getColorFromServer(){
   if (WiFi.status() == WL_CONNECTED)
   {
     HTTPClient http;
-    http.begin(serverUrl);        // kết nối tới server trên máy tính
-    int httpCode = http.POST(""); // gửi POST request
+    http.begin(serverUrl + "/data"); // kết nối tới server trên máy tính
+    int httpCode = http.POST("");    // gửi POST request
 
     if (httpCode > 0)
     {                                    // kiểm tra HTTP response
@@ -60,20 +77,10 @@ void loop()
       {
         // lấy các thuộc tính của đèn
         JsonObject obj = doc["obj"];
-        int level = obj["level"];
-        int r = obj["r"];
-        int g = obj["g"];
-        int b = obj["b"];
-        Serial.printf("r=%d, g=%d, b=%d, level=%d\n", r, g, b, level);
-        // float sr = 0.0015;
-        // float sg = 0.0008;
-        // float sb = 0.00005;
-        // r = (sr / 0.299 * 220 * r / 255 + 2) / 3.3 * 255 * (10 + level - 4) / 10;
-        // g = (sg / 0.587 * 220 * g / 255 + 3) / 3.3 * 255 * (10 + level - 4) / 10;
-        // r = (sb / 0.114 * 220 * b / 255 + 3.2) / 3.3 * 255 * (10 + level - 4) / 10;
-        r=r*(float)0.38*(10+level-4)/10;
-        g=g*(float)0.19*(10+level-4)/10;
-        b=b*(float)1*(10+level-4)/10;
+        level = obj["level"];
+        r = obj["r"];
+        g = obj["g"];
+        b = obj["b"];
         Serial.printf("r=%d, g=%d, b=%d, level=%d\n", r, g, b, level);
         setColor(r, g, b);
       }
@@ -85,5 +92,4 @@ void loop()
 
     http.end(); // đóng kết nối
   }
-  delay(2000); // gửi request mỗi 5 giây
 }
